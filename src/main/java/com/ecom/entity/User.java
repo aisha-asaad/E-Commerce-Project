745cc2e2a19912a72267eaf6adc5814948d44e38
aisha <aishaasaad212@gmail.com>
2025-01-16 15:27:13 +0200

    Initial commit

---
+++ src/main/java/com/ecom/entity/User.java
@@ -0,0 +1,27 @@
+package com.ecom.entity;
+
+import com.ecom.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String name;
+
+    private String password;
+
+    private UserRole role;
+
+    @Lob
+    @Column(columnDefinition = "longblob")
+    private byte[] image;
+
+}
